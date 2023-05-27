a27750f550ff79811b75009b23d70e9e6dbc096c
wzjcad <41847736+wzjcad@users.noreply.github.com>
2023-05-27 10:31:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,14 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
+# Define My Package
+#git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
+#git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
+#git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/molun/luci-theme-edge
+
+
 #更换内核 5.4  5.10 5.15
 sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
